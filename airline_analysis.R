54f56c7796927fa375118b7e12a350838c8841c3
Matt Bhagat-Conway <matt@indicatrix.org>
2022-11-11 20:10:09 -0500

    add ticketing carrier

--- airline_analysis.R
+++ airline_analysis.R
@@ -1,5 +1,7 @@
-# Here, we want to look at what markets are most dominated by which airlines,
-# using the same data
+# Here, we want to look at what airports are most dominated by which airlines,
+# using the same data. For simplicity, we only look at departing flights. Since
+# most departing flights have a corresponding return flight, this should be fairly
+# accurate.
 
 library(tidyverse)
 
@@ -20,12 +22,31 @@ data = left_join(data, rename(market_ids, OriginCity="Description"), by=c(Origin
 data = left_join(data, rename(market_ids, DestCity="Description"), by=c(DestCityMarketID="Code"))
 
 carriers = read_csv("data/L_CARRIERS.csv")
-data = left_join(data, rename(carriers, CarrierName="Description"), by=c(OpCarrier="Code"))
+data = left_join(data, rename(carriers, OperatingCarrierName="Description"), by=c(OpCarrier="Code"))
+data = left_join(data, rename(carriers, TicketingCarrierName="Description"), by=c(TkCarrier="Code"))
 
-mkt_shares = group_by(data, CarrierName, OriginCity) %>%
+# Now, we can compute the market shares
+
+mkt_shares = group_by(data, OperatingCarrierName, OriginCity) %>%
   summarize(Passengers=sum(Passengers)) %>%
   group_by(OriginCity) %>%
   mutate(market_share=Passengers/sum(Passengers), total_passengers=sum(Passengers)) %>%
   ungroup()
 
 filter(mkt_shares, total_passengers > 1000) %>% arrange(-market_share)
+
+# many of the smaller airlines actually operate regional aircraft for larger carriers
+# For instance, PSA Airlines flies small aircraft for American Airlines, branded as
+# American Eagle and sold with connections to/from American Airlines flights.
+# Here, we repeat the analysis using the TicketingCarrierName instead of the
+# OperatingCarrierName.
+
+ticketing_mkt_shares = group_by(data, TicketingCarrierName, OriginCity) %>%
+  summarize(Passengers=sum(Passengers)) %>%
+  group_by(OriginCity) %>%
+  mutate(market_share=Passengers/sum(Passengers), total_passengers=sum(Passengers)) %>%
+  ungroup()
+
+filter(ticketing_mkt_shares, total_passengers > 1000) %>% arrange(-market_share)
+
+# American is much more dominant in Charlotte than before, for example
